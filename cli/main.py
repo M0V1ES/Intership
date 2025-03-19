1cf159e0d5d4ef832dd356e8c7e6371673cabaee
M0V1ES <valera.lomov2016@yandex.ru>
2025-03-19 10:58:26 +0300

    create path

--- cli/main.py
+++ cli/main.py
@@ -5,6 +5,7 @@ from HTTPRequest import HTTPRequest
 from config import config
 import logging
 
+# Создаем параметры для командной строки
 parser = argparse.ArgumentParser(description="CLI-приложение для отправки SMS в API")
 parser.add_argument("your_tel", type=str, help="Введите ваш телефон")
 parser.add_argument("req_tel", type=str, help="Введите телефон для отправки SMS")
@@ -12,6 +13,7 @@ parser.add_argument("text", type=str, help="Введите сообщение д
 args = parser.parse_args()
 
 
+# Отправляем сообщение и принимаем ответ
 def send_message(request: HTTPRequest) -> HTTPResponse:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((request.host, int(request.port)))
@@ -19,23 +21,23 @@ def send_message(request: HTTPRequest) -> HTTPResponse:
         return HTTPResponse.from_bytes(s.recv(4096))
 
 
+# Создаем параметры для логирования, а также файл
 logging.basicConfig(
     level=logging.INFO,
     filename="logfile.log",
     filemode="a",
     encoding="UTF-8",
 )
-
-HOST, PORT = str(config["sms_service"]["url"][:-1]).split("://")[1].split(":")
-req = HTTPRequest(args.your_tel, args.req_tel, args.text, HOST, int(PORT))
-logging.info((args.your_tel, args.req_tel, args.text))
+# Из конфига находим хост, порт и путь
+HOST, PORT = str(config["sms_service"]["url"]).split("://")[1].split(":")
+PATH = PORT.split("/")[1]
+PORT = PORT.split("/")[0]
+req = HTTPRequest(args.your_tel, args.req_tel, args.text, HOST, int(PORT), PATH)
 
 try:
-
     res = send_message(req)
     logging.info(res.status_code)
     print(res.status_code)
-
 except ConnectionRefusedError as e:
     logging.error("Address not found")
     print(e)
