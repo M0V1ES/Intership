5e0480ed0876ee8c0cfae6db4af1b09c4b28da32
M0V1ES <valera.lomov2016@yandex.ru>
2025-03-19 10:57:41 +0300

    create new rules to phone number and update path

--- cli/HTTPRequest.py
+++ cli/HTTPRequest.py
@@ -1,28 +1,46 @@
 import base64
 import json
+import logging
 from dataclasses import dataclass
 from config import config
 
 
 @dataclass
 class HTTPRequest:
-    your_tel: int
-    req_tel: int
+    your_tel: str
+    req_tel: str
     text: str
     host: str
     port: int
+    path: str
+
+    # Создаем правила для номеров и логируем их
+    def __post_init__(self):
+        logging.info((self.your_tel, self.req_tel, self.text))
+        if (len(self.your_tel) == 11) and (len(self.req_tel) == 11):
+            try:
+                your_tel = int(self.your_tel)
+                req_tel = int(self.req_tel)
+
+            except ValueError as e:
+                logging.error(ValueError("Неправильный номер телефона"))
+                raise ValueError("Неправильный номер телефона")
+        else:
+            logging.error(ValueError("Номер телефона должен быть длиной 11 символов"))
+            raise ValueError("Номер телефона должен быть длиной 11 символов")
 
     def to_bytes(self) -> bytes:
+        # Добавляем тело запроса
         data = {
             "sender": self.your_tel,
-            "receiver": self.req_tel,
+            "recipient": self.req_tel,
             "message": self.text,
         }
         body = json.dumps(data)
         # Добавляем заголовки
         headers = [
-            f"POST / HTTP/1.1",
-            f"Host: {self.host}:{self.port}",
+            f"POST /{self.path} HTTP/1.1",
+            f"Host: {self.host}:{self.port}/{self.path}/",
             "Content-Type: application/json",
             f"Content-Length: {len(body)}",
             f"Authorization: Basic {base64.b64encode(f'{config['sms_service']['username']}:{config['sms_service']['password']}'.encode()).decode()}",
@@ -30,7 +48,6 @@ class HTTPRequest:
         ]
         # Собираем весь запрос
         request_parts = "\r\n".join(headers) + "\r\n\r\n" + body
-
         return request_parts.encode()
 
     @classmethod
@@ -41,10 +58,9 @@ class HTTPRequest:
         headers, body = response_str.split("\r\n\r\n", 1)
         # Разделяем строку статуса и заголовки
         header_lines = headers.split("\r\n")
-        # Извлекаем заголовки
         headers_dict = {}
         for line in header_lines[1:]:
-            if ": " in line:  # Проверяем, что строка содержит заголовок
+            if ": " in line:
                 key, value = line.split(": ", 1)
                 headers_dict[key] = value
         return headers, body
